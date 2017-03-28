8f52c922b71438eab39040a8277294e0d3e76e69
Matt Campbell <matt@mattcampbell.net>
2017-03-28 19:20:25 -0400

    Add JSX + todomvc template

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
+import '../node_modules/todomvc-common/base.js';
 import './styles/styles.scss';
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render(h) {
+    return <div>
+      <App/>
+      <footer class="info">
+        <p>Double-click to edit a todo</p>
+        <p>Created by <a href="https://mattcampbell.net">Matt Campbell</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
+    </div>;
+  }
+});
